bfc877f85b4b5d4c4170b9267c8cc1ebd011b5d5
Alexander Ryabishchuk <ar@a2software.dk>
2024-09-08 19:38:00 +0300

    add binary search

---
+++ Algorithms/BinarySearch/BinarySearchImplementation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public static class BinarySearchImplementation
+    {
+        public static int BinarySearch(int[] arr, int x)
+        {
+            int low = 0, high = arr.Length - 1;
+            while (low <= high)
+            {
+                // We look for midpoint of the array
+                int mid = low + (high - low) / 2;
+
+                // Check if value on midpoint index is searched value
+                if (arr[mid] == x)
+                    return mid;
+                // Check if value on midpoint index is smaller than searched value
+                if (arr[mid] < x)
+                    low = mid + 1;
+                // If value on midpoint index is bigger than searched value then new highest index has to be changed
+                else
+                    high = mid - 1;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Algorithms/BinarySearch/Program.cs
@@ -0,0 +1,14 @@
+﻿using BinarySearch;
+
+int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+var index = BinarySearchImplementation.BinarySearch(arr, 9);
+
+if (index == -1)
+{
+    Console.WriteLine("Searched index of element hasn't been found");
+} 
+else
+{
+    Console.WriteLine($"Searched index of element is: {index}");
+}
